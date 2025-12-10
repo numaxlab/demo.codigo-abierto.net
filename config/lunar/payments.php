727ecadf6aadbebde568992a9504a5c434dffbf7
Adrián P. Blunier <adrian@numax.org>
2025-12-10 11:16:00 +0100

    Configured payment methods

--- config/lunar/payments.php
+++ config/lunar/payments.php
@@ -5,9 +5,33 @@ return [
     'default' => env('PAYMENTS_TYPE', 'cash-in-hand'),
 
     'types' => [
-        'cash-in-hand' => [
+        'cash-on-delivery' => [
             'driver' => 'offline',
             'authorized' => 'payment-offline',
+        ],
+        'direct-debit' => [
+            'driver' => 'offline',
+            'authorized' => 'payment-offline',
+        ],
+        'transfer' => [
+            'driver' => 'offline',
+            'authorized' => 'payment-offline',
+        ],
+        'credit' => [
+            'driver' => 'offline',
+            'authorized' => 'payment-offline',
+        ],
+        'card' => [
+            'driver' => 'redsys',
+            'authorized' => 'payment-received',
+        ],
+        'bizum' => [
+            'driver' => 'redsys',
+            'authorized' => 'payment-received',
+        ],
+        'paypal' => [
+            'driver' => 'paypal',
+            'authorized' => 'payment-received',
         ],
     ],
 
